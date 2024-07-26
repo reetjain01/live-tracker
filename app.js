07d96c2b2d116f374cdae9d30bd4ff92896e9a3b
reejain01 <jainreet112@gmail.com>
2024-07-26 21:53:42 +0530

    live tracker initial commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const http = require("http");
+const path = require("path");
+const socketio = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+io.on("connection", (socket) => {
+  socket.on("send-location", (data) => {
+    io.emit("receive-location", { id: socket.id, ...data });
+  });
+  socket.on("disconnect", () => {
+    io.emit("user-disconnected", socket.id)
+  })
+});
+
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, "public")));
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+server.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
